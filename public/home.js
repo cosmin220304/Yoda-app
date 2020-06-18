3489bdf8f256114ab1cd2473293f68500693c5ac
Cosmin <cosmin.aftanase@yahoo.com>
2020-06-19 02:00:37 +0300

    home scene part1

--- public/home.js
+++ public/home.js
@@ -1,3 +1,14 @@
-$(document).ready(function(){
-    $('p').on("click", (e) => $(event.target).text("merge")) 
+const head_pos_up = $('#head').css( "top" ).split('px')[0]
+const head_pos_down = head_pos_up * 1.25
+
+$(document).ready(function(){ 
+    head_move_down()
 });
+
+const head_move_down = () => {
+    $('#head').animate( {top : head_pos_up + 'px'} , 1000, head_move_up)
+}
+
+const head_move_up = () => {
+    $('#head').animate( {top : head_pos_down + 'px'} , 1000, head_move_down)
+}
